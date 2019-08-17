e54522f7a56449847ea0e5f660f4e6d132081842
Vijayaraghavan P <188923@cognizant.com>
2019-08-17 14:38:59 +0530

    debugging info added, error fixes

--- cnn/functional_model.py
+++ cnn/functional_model.py
@@ -39,13 +39,14 @@ class FunctionalModel:
         
             with tf.name_scope('inputAndOutput') as scope:
                 self.X = tf.compat.v1.placeholder(tf.float32, shape=[None, 151, 128, 1], name="X")
-                self.Y = tf.compat.v1.placeholder(tf.float32, [None, 26*26], name="Y")
+                # self.Y = tf.compat.v1.placeholder(tf.float32, [None, 26*26], name="Y")
+                self.Y = tf.compat.v1.placeholder(tf.float32, [None, 3], name="Y")
                 x = self.X
                 
             with tf.name_scope('conv1') as scope:
-                kernelInitializer = keras.initializers.Constant(value=0)
+                kernelInitializer = keras.initializers.Constant(value=1)
                 self.conv1 = tf.compat.v1.keras.layers.Conv2D(
-                    filters=1, 
+                    filters=5, 
                     kernel_size=(3, 3),
                     strides=(1, 1),
                     activation=None, 
@@ -62,24 +63,36 @@ class FunctionalModel:
                 print("pool1 shape", self.pool1.shape)
                 # print("conv1Flattening shape", self.conv1Flattening.shape)
                 
-            # with tf.name_scope('conv2') as scope:
-            #     self.conv2 = tf.compat.v1.keras.layers.Conv1D(filters=5, kernel_size=4, activation=None)(x)
-            #     self.pool2 = tf.compat.v1.keras.layers.MaxPooling1D(pool_size=4)(self.conv2)
-            #     x = self.pool2
+            with tf.name_scope('conv2') as scope:
+                kernelInitializer = keras.initializers.Constant(value=1)
+                self.conv2 = tf.compat.v1.keras.layers.Conv2D(
+                    filters=1, 
+                    kernel_size=(3, 3),
+                    strides=(1, 1),
+                    activation=None, 
+                    data_format='channels_last',
+                    kernel_initializer=kernelInitializer)(x)
+                self.pool2 = tf.compat.v1.keras.layers.MaxPooling2D(
+                    pool_size=(2,2),
+                    strides=(2,2),
+                    padding="valid")(self.conv2)
+                x = self.pool2
                 
-            #     print("conv2 shape", self.conv2.shape)
-            #     print("pool2 shape", self.pool2.shape)
+                print("conv2 shape", self.conv2.shape)
+                print("pool2 shape", self.pool2.shape)
                 
             with tf.name_scope('flattening') as scope:
                 self.flattenedLayer = tf.compat.v1.layers.Flatten(name="flattening")(x)
                 x = self.flattenedLayer
+                self.normalizedLayer = self.normalizeTensor(self.flattenedLayer)
+                x = self.normalizedLayer
 
             with tf.name_scope('fullyConnectedLayerOne') as scope:
                 print("x.shape", x.shape)
-                self.fc1InitialWeights = tf.random.truncated_normal([4662, 26 * 26], stddev=1e-1, name="fc1InitialWeights")
+                self.fc1InitialWeights = tf.random.truncated_normal([1080, 3], stddev=1e-1, name="fc1InitialWeights")
                 self.fc1Weight = tf.Variable(self.fc1InitialWeights, name="fc1Weights")
 
-                self.fc1InitialBias = tf.constant(1.0, shape=[26 * 26], name="fc1InitialBias")
+                self.fc1InitialBias = tf.constant(0.0, shape=[3], name="fc1InitialBias")
                 self.fc1Bias = tf.Variable(self.fc1InitialBias, trainable=True, name="fc1Bias")
 
                 matMulAddBias = tf.nn.bias_add(tf.matmul(x, self.fc1Weight), self.fc1Bias)
@@ -90,32 +103,58 @@ class FunctionalModel:
 
             # with tf.name_scope('fullyConnectedLayerTwo') as scope:
             #     print("x.shape", x.shape)
-            #     self.fc2InitialWeights = tf.random.truncated_normal([26 * 26 * 4, 26 * 26], name="fc2InitialWeights")
+            #     self.fc2InitialWeights = tf.random.truncated_normal([500, 1], name="fc2InitialWeights")
             #     self.fc2Weight = tf.Variable(self.fc2InitialWeights, name="fc2Weights")
 
-            #     self.fc2InitialBias = tf.constant(1.0, shape=[26 * 26], name="fc2InitialBias")
+            #     self.fc2InitialBias = tf.constant(0.0, shape=[1], name="fc2InitialBias")
             #     self.fc2Bias = tf.Variable(self.fc2InitialBias, trainable=True, name="fc2Bias")
 
-            #     matMulAddBias = tf.nn.bias_add(tf.matmul(x, self.fc2Weight), self.fc2Bias)
+            #     matMulAddBias = tf.nn.bias_add(tf.matmul(self.fc1, self.fc2Weight), self.fc2Bias)
             #     self.fc2 = tf.math.sigmoid(matMulAddBias, name="fc2")
             #     self.Ylogits = self.fc2
 
             with tf.name_scope('crossEntropy'):
                 self.crossEntropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.Ylogits, labels=self.Y)
+                # self.crossEntropy = tf.keras.losses.BinaryCrossentropy()(self.Y, self.Ylogits)
                 self.lossFunction = tf.reduce_mean(self.crossEntropy)
+                # self.lossFunction = tf.keras.losses.MSE(self.Y, self.Ylogits)
 
             with tf.name_scope('accuracy'):
-                self.correctPrediction = tf.equal(tf.argmax(self.Ylogits, 0), tf.argmax(self.Y, 0))
+                self.correctPrediction = tf.equal(self.Ylogits, self.Y)
                 self.accuracy = tf.reduce_mean(tf.cast(self.correctPrediction, tf.float32))
 
             with tf.name_scope('train'):
                 self.trainStep = tf.compat.v1.train.AdamOptimizer(learning_rate=.1).minimize(self.lossFunction)
             
+    def normalizeTensor(self, inputTensor):
+        # minTensor = tf.reduce_min(inputTensor, axis=1)
+        # maxTensor = tf.reduce_max(inputTensor, axis=1)
+
+        return  tf.map_fn(
+            lambda x: tf.subtract(
+                tf.multiply(
+                    tf.div(
+                        tf.subtract(
+                            x,
+                            tf.reduce_min(x)
+                        ),
+                        tf.subtract(
+                            tf.reduce_max(x),
+                            tf.reduce_min(x)
+                        )
+                    ),
+                    tf.constant(2.0, shape=[1])
+                ),
+                tf.constant(1.0, shape=[1])
+            ),
+            inputTensor
+        )
+
     
     def train(self, xTrain, yTrain):
         # print("yTrain", yTrain)
 
-        noOfEpoch = 2
+        noOfEpoch = 5
 
         self.sess = tf.compat.v1.Session()
         init = tf.compat.v1.global_variables_initializer()
@@ -131,24 +170,64 @@ class FunctionalModel:
             # print("indices", indices)
             shuffledIndices = tf.random.shuffle(indices)
             # print("shuffledIndices", self.sess.run(shuffledIndices))
-            # mappedShuffledIndices = numpy.fromiter(map(lambda x: numpy.array([x]), self.sess.run(shuffledIndices)), dtype=object)
-            # print("mappedShuffledIndices", mappedShuffledIndices)
 
             xShuffled = []
             yShuffled = []
-            for si in self.sess.run(shuffledIndices):
+            # for si in self.sess.run(shuffledIndices):
+            for si in self.sess.run(indices):
                 xShuffled.append(xTrain[si])
                 yShuffled.append(yTrain[si])
 
             xNorm = normalizeData(xShuffled)
             xNorm = numpy.reshape(xNorm, (-1, 151, 128, 1))
-            print("xNormReshaped shape", xNorm.shape)
+            # print("xNormReshaped shape", xNorm.shape)
             yShuffled = numpy.array(yShuffled)
+            # print("yShuffled shape bfore", yShuffled.shape, yShuffled[0])
+
+            yShuffled = yShuffled[:,[0, 59, 8]]
+            print("yShuffled shape", yShuffled.shape)
             self.sess.run(self.trainStep, feed_dict={self.X: xNorm, self.Y: yShuffled})
             xoutput = self.sess.run(self.X, feed_dict={self.X: xNorm, self.Y: yShuffled})
             # print("X outpu", xoutput, xoutput.shape )
-            conv1output = self.sess.run(self.conv1, feed_dict={self.X: xNorm, self.Y: yShuffled})
-            # print("conv1 outpu", conv1output, conv1output.shape)
+
+
+            # normalizedLayer = self.sess.run(
+            #     self.normalizedLayer, 
+            #     feed_dict={self.X: xNorm, self.Y: yShuffled})
+            # print("normalizedLayer outpu", normalizedLayer, normalizedLayer.shape)
+            # flattenedLayer = self.sess.run(
+            #     self.flattenedLayer, 
+            #     feed_dict={self.X: xNorm, self.Y: yShuffled})
+            # print("flattenedLayer outpu", flattenedLayer, flattenedLayer.shape)
+            # reduceMin = self.sess.run(
+            #     tf.reduce_min(self.flattenedLayer), 
+            #     feed_dict={self.X: xNorm, self.Y: yShuffled})
+            # print("reduceMin outpu", reduceMin, reduceMin.shape)
+            # reduceMax = self.sess.run(
+            #     tf.reduce_max(self.flattenedLayer), 
+            #     feed_dict={self.X: xNorm, self.Y: yShuffled})
+            # print("reduceMax outpu", reduceMax, reduceMax.shape)
+            Ylogits = self.sess.run(
+                self.Ylogits, 
+                feed_dict={self.X: xNorm, self.Y: yShuffled})
+            Y = self.sess.run(
+                self.Y, 
+                feed_dict={self.X: xNorm, self.Y: yShuffled})
+            equal = self.sess.run(
+                tf.equal(self.Ylogits, self.Y),
+                feed_dict={self.X: xNorm, self.Y: yShuffled})
+            reduceMean = self.sess.run(
+                tf.reduce_mean(tf.cast(tf.equal(self.Ylogits, self.Y), tf.float32)),
+                feed_dict={self.X: xNorm, self.Y: yShuffled})
+            print("equal outpu", Ylogits, Y, equal, reduceMean, Ylogits.shape)
+            # lossFunction = self.sess.run(
+            #     self.lossFunction, 
+            #     feed_dict={self.X: xNorm, self.Y: yShuffled})
+            # print("lossFunction outpu", lossFunction, lossFunction.shape)
+            correctPrediction = self.sess.run(
+                self.correctPrediction, 
+                feed_dict={self.X: xNorm, self.Y: yShuffled})
+            print("correctPrediction outpu", correctPrediction, correctPrediction.shape)
 
             acc = self.sess.run([self.accuracy], feed_dict={self.X: xNorm, self.Y: yShuffled})
             accHist.append(acc)
@@ -160,12 +239,14 @@ class FunctionalModel:
 
     def predict(self, xPredict):
         xp = normalizeData(xPredict)
+        xp = numpy.reshape(xp, (-1, 151, 128, 1))
         yPredictions = self.sess.run(self.Ylogits, feed_dict={self.X: xp})
 
         yApproximation = []
         for y in yPredictions:
-            # printOutput(y)
-            yApproximationItem = [1 if yVal > .9 else 0 for yVal in y]
+            printOutput(y)
+            # yApproximationItem = [1 if yVal > .9 else 0 for yVal in y]
+            yApproximationItem = [yVal for yVal in y]
             yApproximation.append(yApproximationItem)
 
         # print("yApproximation", yApproximation)
@@ -179,9 +260,14 @@ class FunctionalModel:
             self.trainLogWriter.add_graph(self.sess.graph)
             
 
-            tf.compat.v1.summary.image("Input", tf.transpose(self.X, perm=[0, 2, 1, 3]))
+            # tf.compat.v1.summary.image("Input", tf.transpose(self.X, perm=[0, 2, 1, 3]))
+            tf.compat.v1.summary.image("Input", self.X, max_outputs=11)
+            # tf.compat.v1.summary.image("Conv1", self.getImageFromConvAt(self.conv1, 0), max_outputs=11)
+            # tf.compat.v1.summary.image("Pool1", self.pool1, max_outputs=11)
+            # tf.compat.v1.summary.image("Conv1", self.conv2, max_outputs=11)
+            tf.compat.v1.summary.image("Pool1", self.pool2, max_outputs=11)
 
-            tf.compat.v1.summary.scalar('Loss', self.lossFunction)
+            # tf.compat.v1.summary.scalar('Loss', self.lossFunction)
             tf.compat.v1.summary.scalar('Accuracy', self.accuracy)
             tf.compat.v1.summary.histogram("fc1Weights", self.fc1Weight)
             tf.compat.v1.summary.histogram("fc1Bias", self.fc1Bias)
@@ -191,6 +277,31 @@ class FunctionalModel:
 
         else:
             raise Exception("Session not available")
+
+    def getImageFromConvAt(self, conv, idx):
+        # return tf.map_fn(
+        #     lambda x: tf.map_fn(
+        #         lambda y: tf.map_fn(
+        #             lambda z: tf.map_fn(
+        #                 lambda a: numpy.array([a[idx]]),
+        #                 z
+        #             ),
+        #             y
+        #         ),
+        #         x
+        #     ),
+        #     conv
+        # )
+         return tf.map_fn(
+            lambda x: tf.map_fn(
+                lambda y: tf.map_fn(
+                    lambda z: numpy.array([z[idx]]),
+                    y
+                ),
+                x
+            ),
+            conv
+        )
 
     def addSummary(self, x, y, epoch):
 
