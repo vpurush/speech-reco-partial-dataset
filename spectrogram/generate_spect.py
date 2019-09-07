fd0f5eb941bbb97c919a94075c8457efe04679c3
Vijayaraghavan P <188923@cognizant.com>
2019-09-07 15:17:10 +0530

    chromagram generation

--- spectrogram/generate_spect.py
+++ spectrogram/generate_spect.py
@@ -1,5 +1,5 @@
 from speechpy import processing as spyproc
-from speechpy import feature as spyfe
+# from speechpy import feature as spyfe
 from matplotlib import pyplot as plt
 from librosa import feature as libfeature
 import librosa.display
@@ -30,12 +30,20 @@ def plotSpectrogram(fileName, spectrogram, transposeData, typ):
 def generateSpectrogram(fileName, audioData, rate):
     # return
     # frames = spyproc.stack_frames(audioData, rate, 0.02, 0.02)
-    # print("frames", frames, frames.shape)
+    # print("frames", fileName, frames.shape, audioData.shape, rate)
 
     # fft = spyproc.fft_spectrum(frames)
-    # print("fft", fft, fft.shape)
+    # fft = numpy.fft.rfft(frames)
+    # print("fft", fft[0], numpy.absolute(fft[0]), numpy.angle(fft[0]), fft.shape)
+
+    # for i in range(0, fft.shape[0]):
+    #     fftF = fft[i]
+    #     var = numpy.var(numpy.absolute(fftF))
+    #     print("var", i, var)
     # print("fft 50", fft[50,:])
     # plotSpectrogram(fileName, fft, True, 'fft')
+
+    # exit()
 
     # power = spyproc.power_spectrum(frames)
     # print("power", power, power.shape)
@@ -65,9 +73,9 @@ def generateSpectrogram(fileName, audioData, rate):
     # Here n_fft is frame length?
     n_fft = int(0.02 * rate)
     # hop_length=n_fft means no overlap between frames
-    melPowerSpectrogram = libfeature.melspectrogram(audioData, rate, S=None, n_fft=n_fft, hop_length=n_fft, n_mels=128)
+    # melPowerSpectrogram = libfeature.melspectrogram(audioData, rate, S=None, n_fft=n_fft, hop_length=int(n_fft / 2), n_mels=128)
     # print("melSpectrogram", melPowerSpectrogram.shape)
-    melDBSpectrogram = librosa.power_to_db(melPowerSpectrogram, ref=numpy.max)
+    # melDBSpectrogram = librosa.power_to_db(melPowerSpectrogram, ref=numpy.max)
     # print("melDBSpectrogram", melDBSpectrogram.shape)
 
     # plotSpectrogram(fileName, 
@@ -84,10 +92,20 @@ def generateSpectrogram(fileName, audioData, rate):
     # plt.title('Mel spectrogram')
     # fig.savefig("./output_img/spect/" + fileName + "_melSpectrogram" + "_spect.png")
     # plt.close(fig)
+
+
+    chromogram = libfeature.chroma_stft(audioData, rate, n_fft=2048, hop_length=1024)
+    print("chromogram", chromogram, chromogram.shape, numpy.amin(chromogram), numpy.amax(chromogram))
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(chromogram, aspect='auto')
+    ax.set_xticks(numpy.arange(0, chromogram.shape[1], 10))
+    fig.suptitle(fileName)
+    fig.savefig("./output_img/spect/" + fileName + "_chromogram.png")
     
-    melDBSpectrogram = numpy.transpose(melDBSpectrogram)
-    # print("melDBSpectrogram after transp", melDBSpectrogram, melDBSpectrogram.shape)
+    # melDBSpectrogram = numpy.transpose(melDBSpectrogram)
+    # print("melDBSpectrogram after transp", melDBSpectrogram.shape)
 
 
-    return melDBSpectrogram
+    # return melDBSpectrogram
+    return numpy.transpose(chromogram)
 
